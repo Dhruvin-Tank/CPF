d5a0fe1e956b7fccb3fddfab2f4f2c6f73136c75
Dhruvin-Tank <dhruvin.tank.ce@gmail.com>
2026-08-29 16:09:14 +0530

    Add files via upload

---
+++ Practical2_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string EnrollmentNumber;
+    string StudentName;
+    string Branch;
+    short int Semester;
+    short int MobileNumber;
+   
+    // Display header of the program
+    cout << "**************************************************" << endl;
+    cout << "STUDENT RECORD MANAGEMENT SYSTEM" << endl;
+    cout << "**************************************************" << endl;
+
+    // Taking student details as input
+    cout << "Enter Enrollment Number :";
+    cin >> EnrollmentNumber;
+    
+    cout << "Enter Student Name :";
+    cin.ignore();
+    getline(cin, studentName);
+
+    cout << "Enter Branch :";
+    cin >> Branch;
+
+    cout << "Enter Semester :";
+    cin >> Semester;
+
+    cout << "Enter Mobile Number :";
+    cin >> MobileNumber;
+
+    // Displaying the entered student information
+    cout << "------------------------------------------" << endl;
+    cout << "Student Information" << endl;
+    cout << "------------------------------------------" << endl;
+
+    cout << " Enrollment Number :" << EnrollmentNumber << endl;
+    cout << " Student Name :" << StudentName << endl;
+    cout << " Branch :" << Branch << endl;
+    cout << " Semester :" << Semester << endl;
+    cout << " Mobile Number :" << MobileNumber << endl;
+
+    cout << "------------------------------------------" << endl;
+
+    return 0;
+}
